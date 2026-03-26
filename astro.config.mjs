0dcbdb8aec30902cd39f586770580feb5bfe81df
jg2867680-tech <jg2867680@gmail.com>
2026-03-26 11:03:17 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // UPDATE THESE TWO LINES
-  site: 'https://<YOUR_USERNAME>.github.io',
-  base: '/<YOUR_REPO_NAME>',
+  // Your main URL
+  site: 'https://edushastra.github.io',
+
+  // Leave this as an empty string for a User Site (edushastra.github.io)
+  base: '', 
   
   vite: {
     plugins: [tailwindcss()],
